8c00b4a325af2d73a4db03d804fc7630d15b89f2
aster <192608617@qq.com>
2026-04-16 23:11:22 +0800

    x

---
+++ compilers/xcell-config/src/index.ts
@@ -0,0 +1,29 @@
+export type {
+    LineConfig,
+    BoolTypeConfig,
+    TypeConfig,
+    MergeMode,
+    MergeRule,
+    GeneratorType,
+    GeneratorConfig,
+    CocosCodegenConfig,
+    StorageConfig,
+    UnityCodegenConfig,
+    ProjectConfig,
+} from "./interfaces";
+
+export {
+    createDefaultLineConfig,
+    createDefaultBoolTypeConfig,
+    createDefaultTypeConfig,
+    createDefaultProjectConfig,
+} from "./defaults";
+
+export {
+    loadProjectConfig,
+    loadProjectConfigFromDir,
+} from "./loader";
+
+export {
+    validateProjectConfig,
+} from "./validator";
